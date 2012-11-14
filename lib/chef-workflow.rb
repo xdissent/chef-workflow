383cda225728b551d9811cae5a8f6c9473580b62
Erik Hollensbe <erik+github@hollensbe.org>
2012-11-14 10:07:50 -0800

    first stab at an "easy configure" helper.

--- lib/chef-workflow.rb
+++ lib/chef-workflow.rb
@@ -5,4 +5,30 @@ require 'chef-workflow/vagrant-support'
 require 'chef-workflow/ip-support'
 require 'chef-workflow/debug-support'
 
-#require 'chef-workflow/config-dsl'
+class Chef
+  module Workflow
+    module ConfigureHelper
+      def configure_knife(&block)
+        KnifeSupport.configure(&block)
+      end
+
+      def configure_vagrant(&block)
+        VagrantSupport.configure(&block)
+      end
+
+      def configure_ips(&block)
+        IPSupport.configure(&block)
+      end
+    end
+  end
+end
+
+class << eval("self", TOPLEVEL_BINDING)
+  include Chef::Workflow::ConfigureHelper
+end
+
+if defined? Rake::DSL
+  module Rake::DSL
+    include Chef::Workflow::ConfigureHelper
+  end
+end
