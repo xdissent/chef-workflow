7ba76cf3fba03b9aded2270dd9a6dfadf1d8f966
Erik Hollensbe <erik+github@hollensbe.org>
2012-11-15 12:36:39 -0800

    if_debug now takes an optional else proc which will be executed if the debug condition isn't met. this makes writing some switching logic easier.

--- lib/chef-workflow/debug-support.rb
+++ lib/chef-workflow/debug-support.rb
@@ -1,10 +1,12 @@
 module DebugSupport
-  def if_debug(minimum=1)
+  def if_debug(minimum=1, else_block=nil)
     $CHEF_WORKFLOW_DEBUG ||= ENV["CHEF_WORKFLOW_DEBUG"].to_i
     $CHEF_WORKFLOW_DEBUG ||= 0
 
     if $CHEF_WORKFLOW_DEBUG >= minimum
       yield if block_given?
+    elsif else_block
+      else_block.call
     end
   end
 end
