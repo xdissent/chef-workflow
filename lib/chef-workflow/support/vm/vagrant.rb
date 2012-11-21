2cc2f3ccdf4becdebcb2484d263cf0428ef04c83
Erik Hollensbe <erik+github@hollensbe.org>
2012-11-21 00:46:39 -0800

    move responsibility for prison/ip allocation to VM provisioner. see 302b47a464854cb6e43df72d1f191df028a54b70 in chef-workflow-testlib

--- lib/chef-workflow/support/vm/vagrant.rb
+++ lib/chef-workflow/support/vm/vagrant.rb
@@ -1,12 +1,21 @@
+require 'chef-workflow/support/vagrant'
+require 'chef-workflow/support/ip'
 require 'chef-workflow/support/vm'
+require 'vagrant/prison'
 
 class VM::VagrantProvisioner
   attr_reader :prison
-  attr_reader :ips
+  attr_reader :number_of_servers
+  attr_accessor :name
 
-  def initialize(prison, ips)
-    @prison = prison
-    @ips = ips
+  def initialize(name, number_of_servers)
+    @prison             = nil
+    @name               = name
+    @number_of_servers  = number_of_servers
+  end
+
+  def ips
+    IPSupport.singleton.get_role_ips(name)
   end
 
   def ui_class
@@ -14,20 +23,30 @@ class VM::VagrantProvisioner
   end
 
   def startup(*args)
+    IPSupport.singleton.delete_role(name)
+
+    @prison = Vagrant::Prison.new(Dir.mktmpdir, false)
+    prison.name = name
+    prison.configure do |config|
+      config.vm.box_url = VagrantSupport.singleton.box_url
+      config.vm.box = VagrantSupport.singleton.box
+      number_of_servers.times do |x|
+        ip = IPSupport.singleton.unused_ip
+        IPSupport.singleton.assign_role_ip(name, ip)
+        config.vm.define "#{name}-#{x}" do |this_config|
+          this_config.vm.network :hostonly, ip
+        end
+      end
+    end
+
     prison.construct(:ui_class => ui_class)
+
     return prison.start ? [ips] : false
   end
 
   def shutdown
     prison.configure_environment(:ui_class => ui_class)
     prison.cleanup
-  end
-
-  def name
-    prison.name
-  end
-
-  def name=(name)
-    prison.name = name
+    IPSupport.singleton.delete_role(name)
   end
 end
