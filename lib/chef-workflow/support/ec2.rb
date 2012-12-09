67fc25c5a92ff9964970d799bb7d618cc2feeeed
Erik Hollensbe <erik+github@hollensbe.org>
2012-12-08 23:57:54 -0800

    patched up AWS configuration to meet the contract in chef-workflow-bootstrap templates

--- lib/chef-workflow/support/ec2.rb
+++ lib/chef-workflow/support/ec2.rb
@@ -23,11 +23,21 @@ class EC2Support
   end
 
   def ec2_obj
-    ec2 = AWS::EC2.new(
-      :access_key_id => ec2.access_key_id,
-      :secret_access_key => ec2.secret_access_key
-    )
 
+    args = 
+      if access_key_id and secret_access_key
+        {
+          :access_key_id => access_key_id,
+          :secret_access_key => secret_access_key
+        }
+      else
+        {
+          :access_key_id => ENV["AWS_ACCESS_KEY_ID"]
+          :secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"]
+        }
+      end
+
+    ec2 = AWS::EC2.new(args)
     ec2.regions[region]
   end
 
