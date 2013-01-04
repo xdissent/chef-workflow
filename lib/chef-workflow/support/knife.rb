bcb812192fae77d9bdeb5097b15302faf7ab718f
Erik Hollensbe <erik+github@hollensbe.org>
2013-01-04 08:52:03 -0800

    big refactors to KnifeSupport, quite a bit simpler and easier to extend.

--- lib/chef-workflow/support/knife.rb
+++ lib/chef-workflow/support/knife.rb
@@ -1,16 +1,38 @@
 require 'fileutils'
 require 'erb'
+require 'singleton'
 require 'chef-workflow/support/generic'
 require 'chef-workflow/support/general'
 require 'chef-workflow/support/debug'
+require 'chef-workflow/support/attr'
 
 module ChefWorkflow
   #
   # Configuration class for chef tooling and SSH interaction. Uses `GenericSupport`.
   #
   class KnifeSupport
-    include ChefWorkflow::GenericSupport
     include ChefWorkflow::DebugSupport
+    extend ChefWorkflow::AttrSupport
+
+    include Singleton
+
+    class << self
+      include Deprecated
+
+      def singleton
+        return instance
+      end
+
+      def configure(&block)
+        instance.instance_eval(&block) if block
+      end
+
+      def method_missing(sym, *args)
+        instance.send(sym, *args)
+      end
+     
+      deprecated :singleton, "ChefWorkflow::KnifeSupport class methods"
+    end
 
     # defaults, yo
     DEFAULTS = {
@@ -33,82 +55,50 @@ module ChefWorkflow
     log_level                :info
     log_location             STDOUT
     node_name                'test-user'
-    client_key               File.join('<%= KnifeSupport.singleton.chef_config_path %>', 'admin.pem')
+    client_key               File.join('<%= KnifeSupport.chef_config_path %>', 'admin.pem')
     validation_client_name   'chef-validator'
-    validation_key           File.join('<%= KnifeSupport.singleton.chef_config_path %>', 'validation.pem')
+    validation_key           File.join('<%= KnifeSupport.chef_config_path %>', 'validation.pem')
     chef_server_url          'http://<%= IPSupport.get_role_ips("chef-server").first %>:4000'
-    environment              '<%= KnifeSupport.singleton.test_environment %>'
+    environment              '<%= KnifeSupport.test_environment %>'
     cache_type               'BasicFile'
-    cache_options( :path => File.join('<%= KnifeSupport.singleton.chef_config_path %>', 'checksums' ))
-    cookbook_path            [ '<%= KnifeSupport.singleton.cookbooks_path %>' ]
+    cache_options( :path => File.join('<%= KnifeSupport.chef_config_path %>', 'checksums' ))
+    cookbook_path            [ '<%= KnifeSupport.cookbooks_path %>' ]
     EOF
 
-    #
-    # Helper method to allow extensions to add attributes to this class. Could
-    # probably be replaced by `AttrSupport`. Takes an attribute name and a
-    # default which will be set initially, intended to be overridden by the user
-    # if necessary.
-    #
-    def self.add_attribute(attr_name, default)
-      ChefWorkflow::KnifeSupport.configure
+    attr_reader :attributes
 
-      DEFAULTS[attr_name] = default # a little inelegant, but it works.
+    def initialize
+      @attributes = { }
 
-      # HACK: no good way to hook this right now, revisit later.
-      str = ""
-      if attr_name.to_s == "knife_config_path"
-        str = <<-EOF
-          def #{attr_name}=(arg)
-            @#{attr_name} = arg
-            ENV["CHEF_CONFIG"] = arg
-          end
-
-          def #{attr_name}(arg=nil)
-            if arg
-              @#{attr_name} = arg
-              ENV["CHEF_CONFIG"] = arg
-            end
-            
-            @#{attr_name}
-          end
-        EOF
-      else
-        str = <<-EOF
-          def #{attr_name}=(arg)
-            @#{attr_name} = arg
-          end
-
-          def #{attr_name}(arg=nil)
-            if arg
-              @#{attr_name} = arg
-            end
-            
-            @#{attr_name}
-          end
-        EOF
-      end
-
-      ChefWorkflow::KnifeSupport.singleton.instance_eval str
-      ChefWorkflow::KnifeSupport.singleton.send(attr_name, default)
-    end
-    
-    DEFAULTS.each { |key, value| add_attribute(key, value) }
-
-    def initialize(options={})
       DEFAULTS.each do |key, value|
-        instance_variable_set(
-          "@#{key}", 
-          options.has_key?(key) ? options[key] : DEFAULTS[key]
-        )
+        add_attribute(key, value)
       end
+    end
+      
+    #
+    # Helper method to allow extensions to add attributes to this class.
+    # Takes an attribute name and a default which will be set initially,
+    # intended to be overridden by the user if necessary.
+    #
+    #--
+    # FIXME Move all the user-configurable stuff to its own support system
+    #++
+    def add_attribute(attr_name, default)
+      @attributes[attr_name] = default
     end
 
     def method_missing(sym, *args)
-      if_debug(2) do
-        $stderr.puts "#{self.class.name}'s #{sym} method was referenced while trying to configure #{self.class.name}"
-        $stderr.puts "#{self.class.name} has not been configured to support this feature."
-        $stderr.puts "This is probably due to it being dynamically added from a rake task, and you're running the test suite."
-        $stderr.puts "It's probably harmless. Expect a better solution than this debug message soon."
+      attr_name = sym.to_s.sub(/=$/, '').to_sym
+      assignment = sym.to_s.end_with?("=")
+
+      if @attributes.has_key?(attr_name)
+        if assignment or args.count > 0
+          @attributes[attr_name] = args[0]
+        else
+          @attributes[attr_name]
+        end
+      else
+        raise "Attribute #{attr_name} does not exist"
       end
     end
 
@@ -122,3 +112,5 @@ module ChefWorkflow
     end
   end
 end
+
+ChefWorkflow::KnifeSupport.configure
